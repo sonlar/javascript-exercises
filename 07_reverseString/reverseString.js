1dde8ae4f53e6726fb2b8b263bb4e6c9a0b0090c
sonlar <31799749+sonlar@users.noreply.github.com>
2025-05-24 21:13:02 +0200

    Complete task 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  reversed = ""
+  for (char of str) {
+    reversed = char + reversed
+  }
+  return reversed
 };
 
 // Do not edit below this line
