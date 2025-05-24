00dbf8ec915f04b372cef1e662fb7f1e8d81b35d
sonlar <31799749+sonlar@users.noreply.github.com>
2025-05-24 20:34:14 +0200

    Complete 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeats) {
+  let repeatString = ""
+  for (let i = 1; i <= repeats; i++) {
+    repeatString += str
+  }
+  return repeatString
 };
 
 // Do not edit below this line
